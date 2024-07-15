a748c68ea7c8f8272d94ca717837d91589cd828c
Martin Racana <ing.racana@gmail.com>
2024-07-15 22:01:25 +0200

    ref v2

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,24 +3,20 @@ import { generateGasPrices } from "../utils/helpers";
 import { GasPrices } from "./types";
 import { updateDataEvery } from "./constants";
 
-/* To keep the same return value while the prices are the same.
- to avoid using a setTimeout that will delay the response x seconds.
-
- This way every request has a quick response and change the value after given time frame.
- */
-
-let gasPrices: GasPrices[] = generateGasPrices();
+let gasPrices: GasPrices = generateGasPrices(); // Adjusted type to GasPrices
 let lastUpdated = Date.now();
 
 export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
 
+  // Use default update interval or the one provided in the request
   const updateData = Number(searchParams.get("update")) || updateDataEvery;
 
   const now = Date.now();
   if (now - lastUpdated > updateData * 1000) {
-    // 5 seconds
+    // Update gas prices if the specified time frame has passed
     gasPrices = generateGasPrices();
     lastUpdated = now;
   }
